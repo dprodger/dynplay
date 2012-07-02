5ad3b15208a6455e3f832cf244d17a03eaef85ba
Aaron Daubman <daubman@gmail.com>
2012-07-02 08:57:53 -0400

    fix app reload issue if song is specified

--- js/enplayer.js
+++ js/enplayer.js
@@ -1,3 +1,5 @@
+/*jshint sub: true */
+
 var enToSpotIds = {};
 
 var apiKey = "N6E4NIOVYMTHNDM8J";
@@ -175,30 +177,29 @@ function makePlaylist() {
 	}
 }
 
-function getSongIDFromTitle( artist, songTitle, artistHot, songHot, variety ) {
-	var url = "http://" + apiHost + "/api/v4/song/search?api_key=" + apiKey + "&callback=?";
+function getSongIDFromTitle(artist, songTitle, artistHot, songHot, variety) {
+    var url = "http://" + apiHost + "/api/v4/song/search?api_key=" + apiKey + "&callback=?";
 
-	$.getJSON( url, 
-		{
-			'artist': artist,
-			'title': songTitle,
-			'format':'jsonp'
+    $.getJSON(url,
+        {
+            'artist':artist,
+            'title':songTitle,
+            'format':'jsonp'
 //			'bucket': ['tracks', 'id:spotify-WW'],
 //			'limit': true,
-		}, function(data) {
-				console.log("=== in getSongIDFromTitle; received a response");
-				var response = data.response;
-				var songs = response.songs;
-				var song = songs[0];
-				
-				if( song ) {
-					console.log("=== looking for song: " + songTitle + " and got: " + song.id + " (" + song.title + ")"  );
-				
-					innerGeneratePlaylist( artist, song.id, song.title, artistHot, songHot, variety, false );
-				} else {
-					alert("We can't find that song");
-				}
-			});
+        }, function (data) {
+            console.log("=== in getSongIDFromTitle; received a response");
+            var response = data.response;
+            var songs = response.songs;
+            if (songs && songs.length > 0) {
+                var song = songs[0];
+                console.log("=== looking for song: " + songTitle + " and got: " + song.id + " (" + song.title + ")");
+                innerGeneratePlaylist(artist, song.id, song.title, artistHot, songHot, variety, false);
+            } else {
+                console.log("=== looking for song: " + songTitle + " and did not get any songs back!");
+                alert("We can't find that song");
+            }
+        });
 }
 
 function displayEnterNew() {
